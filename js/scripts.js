d26ef5d5ca6930db52560b3efcbea187ec62f839
nathanhall762 <4943@holbertonstudents.com>
2022-12-15 08:36:07 -0600

    enhance newsApi functionality, ready for next api

--- js/scripts.js
+++ js/scripts.js
@@ -14,27 +14,39 @@ $(document).ready(function () {
   });
   $('button.form-objects').on('click', function () {
     $('.form-container').remove();
+    const countries = [
+      'fr',
+      'hk',
+      'ae',
+      'ar',
+      'at',
+      'be',
+      'bg',
+      'kr'
+    ];
     for (let i = 0; i < 9; i++) {
-      newsApi();
+      newsApi(i, countries[i]);
     }
   });
 });
 
-function newsApi () {
+function newsApi (i, countries) {
   $.ajax({
     url: 'https://newsapi.org/v2/top-headlines',
     type: 'GET',
     data: {
-      country: 'hk',
+      country: countries,
       apiKey: '779b745ff8aa4d5a960d37036412c454',
-      pageSize: 1,
+      pageSize: 9,
       page: 1
     },
     success: function (response) {
     //   let respObj = JSON.parse(response);
-      $('.master-grid').append("<div class='bubble'><div>");
-      $('.bubble').append(response.status)
-      console.log(response.status)
+      $('.master-grid').append("<div class='bubble' id='" + i + "'><div>");
+      const obj = (response.articles[-1 + i]);
+      const objArray = (Object.values(obj));
+      console.log(objArray[5]);
+      $('#' + i).append("<a href='" + objArray[4] + "' target='_blank'><img src='" + objArray[5] + "' alt='incoming bubble...' style='width:20vw;height:20vh;'></a>");
     }
   });
 }
